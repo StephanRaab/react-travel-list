a6393dc1da912407d0b46381de66d4824406e5c5
Stephan Raab <sarcartist@gmail.com>
2024-12-16 15:48:10 -0500

    list updates with item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import './index.css'
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
     return(
         <>
@@ -29,8 +34,18 @@ function Form() {
 
 function PackingList() {
     return (
-        <div className={'list'}>List of items here...</div>
+        <div className={'list'}>
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item}/>
+                ))}
+            </ul>
+        </div>
     )
+}
+
+function Item({item}) {
+    return <li key={item.id}><span>{item.quantity} {item.description}</span></li>
 }
 
 function Stats() {
