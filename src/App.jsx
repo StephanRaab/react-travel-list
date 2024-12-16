ae852faee45a7067591378fe4fd1bb577afc65e1
Stephan Raab <sarcartist@gmail.com>
2024-12-16 15:07:13 -0500

    some css updates and defining the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
 import './index.css'
 
 export default function App() {
+    return(
+        <>
+            <Header/>
+            <Form/>
+            <PackingList/>
+            <Stats/>
+        </>
+        )
+}
+
+function Header() {
   return (
-    <>
       <header>
-        <h1>Travel List</h1>
+        <h1>🏖️ Travel List 🌴</h1>
       </header>
-    </>
   )
 }
 
+function Form() {
+    return (
+        <div className={'add-form'}>
+            <h3>What do you need to pack 👜 for your trip?</h3>
+        </div>
+    )
+}
+
+function PackingList() {
+    return (
+        <div className={'list'}>List of items here...</div>
+    )
+}
+
+function Stats() {
+    return (
+        <footer className={'stats'}><em>X items left to pack, already X% done! ✈️</em></footer>
+    )
+}
