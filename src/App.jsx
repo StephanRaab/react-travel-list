f42d68ac1a94ec1ee153f128b2237b4ed06d7501
Stephan Raab <sarcartist@gmail.com>
2024-12-16 14:51:21 -0500

    git init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>Travel List</h1>
+      </header>
+    </>
+  )
+}
+
